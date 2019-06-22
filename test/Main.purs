47ed7ea0cfbc4d65f7dbe06d1bc26e62739d26e0
bouzuya <m@bouzuya.net>
2019-06-22 16:10:24 +0900

    fix tests

--- test/Main.purs
+++ test/Main.purs
@@ -6,13 +6,10 @@ import Prelude
 
 import Effect (Effect)
 import Test.Bouzuya.HTTP.Header as Header
-import Test.Unit as TestUnit
-import Test.Unit.Assert as Assert
+import Test.Bouzuya.HTTP.Headers as Headers
 import Test.Unit.Main as TestUnitMain
 
 main :: Effect Unit
 main = TestUnitMain.runTest do
   Header.tests
-  TestUnit.suite "dummy" do
-    TestUnit.test "dummy" do
-      Assert.equal 1 1
+  Headers.tests
